4047624326e6539e91b4abc4e824098808b6463f
Mahmudur Rahman Hera <mahmudhera93@gmail.com>
2021-10-28 01:31:53 -0400

    updated

--- test_code.py
+++ test_code.py
@@ -6,5 +6,5 @@ if __name__ == "__main__":
     num_runs = 100
     s = 0.1
     k = 21
-    values = compare_two_files_to_get_multiple_containments(f1, f2, k, s, num_runs)
-    print(values)
+    size_1, size_2, size_union, size_intersection, true_containment, scaled_containments, sketch_sizes = compare_two_files_to_get_multiple_containments(f1, f2, k, s, num_runs)
+    print(scaled_containments)
