83bf2f77ca758f264a503cf22f76d0d2a510d3a8
Mahmudur Rahman Hera <mahmudhera93@gmail.com>
2021-10-28 01:39:35 -0400

    updated

--- test_code.py
+++ test_code.py
@@ -1,3 +1,4 @@
+import subprocess
 from compare_two_genomes import compare_two_files_to_get_multiple_containments
 
 if __name__ == "__main__":
@@ -7,4 +8,17 @@ if __name__ == "__main__":
     s = 0.1
     k = 21
     size_1, size_2, size_union, size_intersection, true_containment, scaled_containments, sketch_sizes = compare_two_files_to_get_multiple_containments(f1, f2, k, s, num_runs)
+    
+    f = open("script.sh", 'w')
+    for i in range(num_runs):
+        seed_for_mash = i + 1
+        command = "mash dist " + f1 + " " + f2 + " -s " +str(sketch_sizes[i])+ " -S " + seed_for_mash
+        f.write(command)
+        f.write("\n")
+    f.close()
+    
+    cmd = "bash script.sh > testout"
+    cmd_args = cmd.split(' ')
+    subprocess.call(cmd_args)
+    
     print(scaled_containments)
