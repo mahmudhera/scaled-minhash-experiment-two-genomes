fb1e6b8a5a9a6974002da733a7e35b89744c08c0
Mahmudur Rahman Hera <mahmudhera93@gmail.com>
2021-10-29 17:34:47 -0400

    updated

--- test_code.py
+++ test_code.py
@@ -2,14 +2,31 @@ import subprocess
 import numpy as np
 from compare_two_genomes import compare_two_files_to_get_multiple_containments
 from p_from_scaled_containment import compute_confidence_interval_one_step
+import argparse
+import sys
+
+def parse_arguments(sys_args):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-k", "--ksize", type=int, default=21)
+    parser.add_argument("-s", "--scale-facor", type=float, default=0.001)
+    parser.add_argument("--f1", default=None)
+    parser.add_argument("--f2", default=None)
+    parser.add_argument("--seed", type=int, default=1)
+    parser.add_argument("-c", "--confidence", default=0.95)
+    parser.add_argument("-N", "--num-runs", default=10)
+    parser.add_argument("-p", "--mutation-rate", type=int, default=-1)
+    args = parser.parse_args()
+    return args
 
 if __name__ == "__main__":
-    f1 = "original.fasta"
-    f2 = "mutated.fasta"
-    num_runs = 10
-    s = 0.01
-    k = 21
-    confidence = 0.95
+    args = parse_arguments(sys.argv)
+    f1 = args.f1
+    f2 = args.f2
+    num_runs = args.num_runs
+    s = args.scale_factor
+    k = args.ksize
+    confidence = args.confidence
+    known_mutation_rate = args.mutation_rate
     
     size_1, size_2, size_union, size_intersection, true_containment, scaled_containments, sketch_sizes = compare_two_files_to_get_multiple_containments(f1, f2, k, s, num_runs)
     
@@ -57,8 +74,6 @@ if __name__ == "__main__":
     scaled_c_avg = np.average(scaled_containments)
     scaled_c_var = np.var(scaled_containments)
     
-    print(true_containment, mash_c_avg, mash_c_var, scaled_c_avg, scaled_c_var)
-    
     # get p from scaled containment
     scaled_containment = scaled_containments[0]
     L = int((size_1 + size_2)/2)
@@ -80,6 +95,7 @@ if __name__ == "__main__":
     f.close()
     
     mash_distance = mash_distances[0]
+    # get true p from fast ani if == -1
     
-    print(mash_distance, conf_interval)
-    # get true p from fast ani
+    print(true_containment, mash_c_avg, mash_c_var, scaled_c_avg, scaled_c_var)
+    print(known_mutation_rate, mash_distance, conf_interval[6], conf_interval[4], conf_interval[5])
