a4365003d12f18ff241a5ff9e6e7434483efe895
Mahmudur Rahman Hera <mahmudhera93@gmail.com>
2021-10-29 12:20:33 -0400

    added p-from-containment

--- test_code.py
+++ test_code.py
@@ -5,8 +5,8 @@ from compare_two_genomes import compare_two_files_to_get_multiple_containments
 if __name__ == "__main__":
     f1 = "original.fasta"
     f2 = "mutated.fasta"
-    num_runs = 100
-    s = 0.1
+    num_runs = 1000
+    s = 0.01
     k = 21
     size_1, size_2, size_union, size_intersection, true_containment, scaled_containments, sketch_sizes = compare_two_files_to_get_multiple_containments(f1, f2, k, s, num_runs)
     
