97e82839eb52e15a55330fe7651194aabfcda3cf
Mahmudur Rahman Hera <mahmudhera93@gmail.com>
2021-10-30 16:06:34 -0400

    updated

--- ecoli_simulated_mutation_experiment.py
+++ ecoli_simulated_mutation_experiment.py
@@ -2,12 +2,12 @@ import subprocess
 from mutate_genome import mutate_file
 
 mut_rates = [0.001, 0.01, 0.025, 0.05, 0.1, 0.15, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.99]
-seed = 1
+seed = 2
 stats_filename = 'stats'
 reference_genome = 'ecoli.fasta'
 mutated_filename_prefix = 'ecoli_mutated_'
 k = 21
-scale_factor = 0.1
+scale_factor = 0.4
 num_runs = 10
 
 for mutation_rate in mut_rates:
